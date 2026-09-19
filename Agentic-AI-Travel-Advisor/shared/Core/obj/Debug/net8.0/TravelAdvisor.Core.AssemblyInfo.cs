20981538da11f5e5372d1554c2284150bddc9b4f
vipooshans <vipoovipooshan9@gmail.com>
2026-09-19 19:12:26 +0530

    Regenerate binaries and assembly info files for TravelAdvisor.Core, TravelAdvisor.Infrastructure, and TravelAdvisor.Api projects, reflecting version updates and source link adjustments. Update debug binaries for web app and web API components.

--- Agentic-AI-Travel-Advisor/shared/Core/obj/Debug/net8.0/TravelAdvisor.Core.AssemblyInfo.cs
+++ Agentic-AI-Travel-Advisor/shared/Core/obj/Debug/net8.0/TravelAdvisor.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelAdvisor.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474f5af4b2ec3364320ebe17f21f54e0627b9417")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed7580a5b9c022605eda8c0ffb60faf6da59a9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelAdvisor.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelAdvisor.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
